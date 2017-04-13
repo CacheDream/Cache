f5ea9848e018583a8fe31b35ce171f79ad8f66a0
Candison <kandisheng@163.com>
2017-04-13 15:58:03 +0800

    Fix Bug

--- application/Helper/Core.php
+++ application/Helper/Core.php
@@ -142,7 +142,7 @@ class Core
             preg_match_all($preg, $content, $result);
             $fileList = $result[1];
             foreach ($fileList as $file) {
-                if (substr(strtolower($file), 0, 5) != 'data:' && substr(strtolower($file), 0, 6) != '"data:' && substr(strtolower($file), 0, 6) != '\'data:') {
+                if (substr(strtolower($file), 0, 5) != 'data:') {
                     $fileNew = $this->mergeURL($url, $file);
                     $fileNew = urlencode($fileNew);
                     if (empty($version)) {
@@ -151,8 +151,6 @@ class Core
                         $fileNew = sprintf('url(/?v=%s&f=%s)', $version, $fileNew);
                     }
                     $content = str_replace(sprintf('url(%s)', $file), $fileNew, $content);
-                } else {
-                    $content = str_replace(sprintf('url(%s)', $file), sprintf('url("%s")', $file), $content);
                 }
             }
         }
